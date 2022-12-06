f6cad9b261de90b8dc383e140f2b5701087d7956
Dragxfly <59048803+JannisJost@users.noreply.github.com>
2022-12-06 14:27:48 +0100

    updated log in page design

--- videotube/lib/model/sign_in.dart
+++ videotube/lib/model/sign_in.dart
@@ -13,34 +13,65 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          ElevatedButton.icon(
-            style: ElevatedButton.styleFrom(
-              primary: Colors.white,
-              onPrimary: Colors.black,
-              minimumSize: Size(double.infinity, 50),
-            ),
-            label: Text("Sign Up with Google"),
-            onPressed: () {
-              googleSignInProvider.signInWithGoogle();
-            },
-            icon: Icon(Icons.person),
+        body: Column(
+      children: <Widget>[
+        SizedBox(
+          height: 20,
+        ),
+        Image.asset(
+          'assets/icons/vitu.png',
+          height: 150,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          'Log in to Vitu to access all features',
+          style: const TextStyle(
+            color: Color(0xffDFDDDD),
+            fontSize: 15,
+            fontWeight: FontWeight.w300,
           ),
-          ElevatedButton.icon(
-            style: ElevatedButton.styleFrom(
-              primary: Colors.white,
-              onPrimary: Colors.black,
-              minimumSize: Size(double.infinity, 50),
-            ),
-            label: Text("Sign out"),
-            onPressed: () {
-              googleSignInProvider.signOutGoogle();
-            },
-            icon: Icon(Icons.lock_person),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Expanded(
+            child: Container(
+          padding: EdgeInsets.only(left: 15, right: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromRGBO(220, 61, 63, 1),
+                  foregroundColor: Colors.white,
+                  minimumSize: Size(double.infinity, 60),
+                ),
+                label: Text("Sign In with Google"),
+                onPressed: () {
+                  googleSignInProvider.signInWithGoogle();
+                },
+                icon: Icon(Icons.person),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromRGBO(220, 61, 63, 1),
+                  foregroundColor: Colors.white,
+                  minimumSize: Size(double.infinity, 60),
+                ),
+                label: Text("Sign out"),
+                onPressed: () {
+                  googleSignInProvider.signOutGoogle();
+                },
+                icon: Icon(Icons.lock_person),
+              ),
+            ],
           ),
-        ],
-      )
-    );
+        )),
+      ],
+    ));
   }
 }
